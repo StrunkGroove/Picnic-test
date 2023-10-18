5d7d365a7a55590e87c5fdf730b4caf4d7b37d65
StrunkGroove <jmarkj@mail.ru>
2023-10-18 04:27:24 +0300

    Решена проблема создания таблиц при первом запуске

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from database.database import init_db
 from api.endpoints.v1.users import router as users_router
 from api.endpoints.v1.picnic import router as picnic_router
 from api.endpoints.v1.city import router as city_router
@@ -7,6 +8,9 @@ from api.endpoints.v1.city import router as city_router
 
 app = FastAPI()
 
+init_db()
+
 app.include_router(users_router, prefix="/api/v1/users", tags=["Users"])
 app.include_router(picnic_router, prefix="/api/v1/picnic", tags=["Picnic"])
 app.include_router(city_router, prefix="/api/v1/city", tags=["City"])
+
